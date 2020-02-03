090044e27380c41cd11fd546961b11e5a9b1ad11
Gelmius Blažys <s030353@ad.viko.lt>
2020-02-03 11:23:34 +0200

    Add project files.

---
+++ DBOldSchool.cs
@@ -0,0 +1,84 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+public class DBOldSchool
+{
+
+    private MySqlConnection connection;
+    private string server;
+    private string database;
+    private string uid;
+    private string password;
+    private string connectionString;
+
+
+
+    //Constructor
+    public DBOldSchool()
+    {
+        Initialize();
+    }
+    public DBOldSchool(string server, string database, string uid, string password)
+    {
+        Initialize(server, database, uid, password);
+    }
+
+    //Initialize values
+    private void Initialize()
+    {
+
+        server = "178.128.202.96";
+        database = "Testas";
+        uid = "TestUser";
+        password = "TestasTesta5";
+        connectionString = "SERVER=" + server + ";port=3306;" + "DATABASE=" +
+        database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";Convert Zero Datetime=true;default command timeout=999;";
+
+        connection = new MySqlConnection(connectionString);
+
+    }
+    private void Initialize(string server, string database, string uid, string password)
+    {
+        connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+        database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";Convert Zero Datetime=true;default command timeout=999;";
+
+        connection = new MySqlConnection(connectionString);
+
+    }
+
+    //open connection to database
+    private bool OpenConnection()
+    {
+        try
+        {
+            connection.Open();
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+    public void OpenConnectionlong()
+    {
+        try
+        {
+            connection.Open();
+        }
+        catch
+        {
+
+        }
+    }
+
+    public DataSet SelectAll(string table)
+    {
+        string query = "SELECT * FROM " + table;
+
+        MySqlDataAdapter adpt = new MySqlDataAdapter(query, connection);
+        DataSet dset = new DataSet();
+        adpt.Fill(dset);
+        return dset;
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using MySql.Data.MySqlClient;
+
+
+namespace mysql1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            DBOldSchool con = new DBOldSchool("178.128.202.96", "Testas", "TestUser", "TestasTesta5");
+
+            Console.WriteLine("write text(case sensetive)");
+            String[] ivestis = Console.ReadLine().Split(" ");
+            DataSet aaa = con.SelectAll("Automobilis");
+            foreach (DataTable table in aaa.Tables)
+            {
+                foreach (DataRow dr in table.Rows)
+                {
+                    int points = 0;
+                    for(int i =0;i<ivestis.Length;i++)
+                    {
+                        if (!(string.Join(",", dr.ItemArray).Split(',').ToList().Contains(ivestis[i])))
+                        {
+                            break;
+                        }
+                        else { points++; }
+                        if(points == ivestis.Length)
+                        {
+                            foreach (DataColumn dc in table.Columns)
+                            {
+                                Console.WriteLine(dc.ToString()+": "+ dr[dc].ToString()+" ");
+                            }
+                            Console.WriteLine();
+                        }
+                    }   
+                }
+            }
+        }   
+    }
+}
